d40fe3fbfc088e3b0a7635e2485b64fed99da22c
dawnzs01 <18013865675@163.com>
2023-10-19 17:03:53 +0800

    gitlab-plugin-error

--- src/test/java/com/dabsquared/gitlabjenkins/trigger/handler/pipeline/PipelineHookTriggerHandlerImplTest.java
+++ src/test/java/com/dabsquared/gitlabjenkins/trigger/handler/pipeline/PipelineHookTriggerHandlerImplTest.java
@@ -13,20 +13,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import com.dabsquared.gitlabjenkins.gitlab.hook.model.PipelineHook;
 import com.dabsquared.gitlabjenkins.gitlab.hook.model.User;
 import com.dabsquared.gitlabjenkins.trigger.filter.BranchFilterType;
+import hudson.Functions;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
-import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.util.OneShotEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ExecutionException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -96,15 +97,13 @@ public class PipelineHookTriggerHandlerImplTest {
     /**
      * always triggers since pipeline events do not contain ci skip message
      */
-    public void pipeline_ciSkip() throws Exception {
+    public void pipeline_ciSkip() throws IOException, InterruptedException {
         final OneShotEvent buildTriggered = new OneShotEvent();
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        final AtomicReference<FreeStyleBuild> buildHolder = new AtomicReference<>();
         project.getBuildersList().add(new TestBuilder() {
             @Override
             public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
                     throws InterruptedException, IOException {
-                buildHolder.set((FreeStyleBuild) build);
                 buildTriggered.signal();
                 return true;
             }
@@ -119,20 +118,17 @@ public class PipelineHookTriggerHandlerImplTest {
 
         buildTriggered.block(10000);
         assertThat(buildTriggered.isSignaled(), is(true));
-        jenkins.assertBuildStatusSuccess(jenkins.waitForCompletion(buildHolder.get()));
     }
 
     @Test
-    public void pipeline_build() throws Exception {
+    public void pipeline_build() throws IOException, InterruptedException, GitAPIException, ExecutionException {
 
         final OneShotEvent buildTriggered = new OneShotEvent();
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        final AtomicReference<FreeStyleBuild> buildHolder = new AtomicReference<>();
         project.getBuildersList().add(new TestBuilder() {
             @Override
             public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
                     throws InterruptedException, IOException {
-                buildHolder.set((FreeStyleBuild) build);
                 buildTriggered.signal();
                 return true;
             }
@@ -148,6 +144,22 @@ public class PipelineHookTriggerHandlerImplTest {
 
         buildTriggered.block(10000);
         assertThat(buildTriggered.isSignaled(), is(true));
-        jenkins.assertBuildStatusSuccess(jenkins.waitForCompletion(buildHolder.get()));
+    }
+
+    @After
+    public void after() {
+        /*
+         * Add Thread.sleep(5000) to avoid the following error on Windows:
+         *
+         *     Unable to delete 'C:\Jenkins\workspace\Plugins_gitlab-plugin_PR-1121\target\tmp\j h4861043637706712359'.
+         *     Tried 3 times (of a maximum of 3) waiting 0.1 sec between attempts.
+         */
+        if (Functions.isWindows()) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
     }
 }
